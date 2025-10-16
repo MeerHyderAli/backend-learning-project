217ff49ae5e22381861770d4f60040b595b1c7ba
hyder.zenexis <hyder.zenexistech@gmail.com>
2025-10-16 15:38:06 +0530

    Utils files added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requesthandler) => {
+  (req, res, next) => {
+    Promise.resolve(requesthandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+export default asyncHandler;
